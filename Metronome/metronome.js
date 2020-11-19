2c3d03ecccae9aa4bf8b9314c92ea09a775c7023
brimason81 <brimason81@gmail.com>
2020-11-19 12:08:42 -0500

    Web Audio:  sound is heard but incorrect interval Cleaned up CSS & HTML a bit. Imported .wav to <audio>

--- Metronome/metronome.js
+++ Metronome/metronome.js
@@ -1,63 +1,72 @@
-// Web Audio API?
-const AudioContext = window.AudioContext || webkitAudioContext;
+// Web Audio API
+const AudioContext = window.AudioContext || window.webkitAudioContext; // CONSTRUCTOR FOR CROSS-BROWSER SUPPORT
+const audioContext = new AudioContext();
 
+// GET AUDIO ELEMENT
+const audioElement = document.querySelector('audio');  
+
+// PASS AUDIO ELEMENT TO THE AUDIO CONTEXT - CREATES INPUT NODE
+const click = audioContext.createMediaElementSource(audioElement);
+click.connect(audioContext.destination); // CONNECT AUDIO GRAPH TO DESTINATION
+
+// ACCESSING html ELEMENTS FOR USE IN JavaScript
 const minusTempo = document.querySelector('.minus');
 const increaseTempo = document.querySelector('.plus');
 const tempoDisplay = document.querySelector('.container-display p');
 const tempoOutput = document.querySelector('.container-dial button');
 const startStopButton = document.querySelector('.control');
 
+// VARIOUS VARIABLES
 var tempo = 60;
 var bpm;
 
 var interval;
 var startStop = false;
 
-// START METRONOME - DISPLAYS, BUT TIMING ISN'T RIGHT; PROBABLY SOMETHING TO DO WITH control()
+// START METRONOME 
 function start() {
     startStop = true;
-    interval = setInterval(control, bpm = (60 / tempo) * 1000); 
-
-    /* FOR TESTING */
-    console.log(tempo);
-    console.log(bpm);
+    interval = setInterval(control, bpm  = (60 / tempo) * 1000); //
 } 
 
 // STOP METRONOME - CLEAR DISPLAY; CLEAR INTERVAL
 function stop() {
     startStop = false;
     clearInterval(interval);
-    
-    /* FOR TESTING */
-    console.log(interval);
-    console.log('all clear');
+    audioElement.pause();
     
     tempoOutput.innerHTML = 'START';
-    tempoOutput.style.backgroundColor = "cyan";
+    tempoOutput.style.backgroundColor = "rgb(22, 255, 1)";
 }
 
 // FUNCTION TO CONTROL tempoOutput INTERVAL
-/*
-    NEED TO FIX THE DISPLAY - WAS WORKING BETTER BEFORE!! NOW JUST FLASHES RAPIDLY DESPITE BPM
-*/ 
 function control() {
+    audioElement.play();
+
     if (tempoOutput.innerHTML != '') {
         tempoOutput.style.backgroundColor = "blueviolet"; 
         tempoOutput.innerHTML = '';
     } else {
         tempoOutput.innerHTML = 'STOP';
-        tempoOutput.style.backgroundColor = "cyan";
+        tempoOutput.style.backgroundColor = "rgb(22, 255, 1)";
     }
 }
 
 // BUTTON TO START AND STOP METRONOME
 startStopButton.addEventListener("click", function() {
+    //StartAudioContext(metronome.audioContext);  MAYBE NOT CORRECT SYNTAX, BUT NOT WORKING CORRECTLY
+    // RESUME AUDIO CONTEXT
+    if (audioContext.state === 'suspended') {
+        audioContext.resume();
+    }
+    
+    // START & STOP AUDIO
     if (startStop == false) { // COULD NOT USE if (!interval)... ; HAD TO USE DIFFERENT CONDITION
         start();
     } else if (startStop == true) {
         stop();
     }
-})
+});
 
 // BUTTON TO DECREASE TEMPO
 minusTempo.addEventListener("click", function(){
@@ -69,23 +78,22 @@ minusTempo.addEventListener("click", function(){
     } else {
         alert('That tempo is too slow!!');
     }
+    /* FOR TESTING */
+    //console.log(bpm);  CHANGE IN TEMPO NOT RECOGNIZED WHILE APP IS RUNNING
 });
 
 // BUTTON TO INCREASE TEMPO
 increaseTempo.addEventListener("click", function(){
     let highestTempo = 208;
     
-    /* FOR TESTING */
-    console.log(bpm); // CHANGE IN TEMPO NOT RECOGNIZED WHILE APP IS RUNNING
-    
     if (tempo < highestTempo) {
         tempo++;
         tempoDisplay.innerHTML = `${tempo}<span> bpm</span>`;
-        
-        
     } else {
         alert('That tempo is too fast!');
     }
+    /* FOR TESTING */
+    //console.log(bpm); CHANGE IN TEMPO NOT RECOGNIZED WHILE APP IS RUNNING
 });
 
 
